da54f8b716d6e905e03aadbb5b689107bc4dd8fa
Michael Efemena <mefemena@gmail.com>
2021-04-12 08:29:54 +0100

    create graphql server with apollo-server-express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import express from "express"
 import { ApolloServer } from "apollo-server-express"
 
+import schema from "./graphql/schemaMap"
+const PORT = 4000
+
+const app = express()
+
+const server = new ApolloServer({
+    schema
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+app.listen(PORT, () => {
+    console.log(`GraphQL is now running on http://localhost:${PORT}/graphql`)
+})
