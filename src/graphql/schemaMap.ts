99301e69e5c77ff66e688629c13854247594ecd5
Michael Efemena <mefemena@gmail.com>
2021-04-12 07:39:59 +0100

    created schema in schemaMap file

--- src/graphql/schemaMap.ts
+++ src/graphql/schemaMap.ts
@@ -1,1 +1,16 @@
 import "graphql-import-node"
+
+import * as userTypeDefs from "./schemas/user.graphql"
+import * as emptyTypeDefs from "./schemas/empty.graphql";
+
+import { makeExecutableSchema } from "graphql-tools"
+import { GraphQLSchema } from "graphql"
+
+import resolvers from "./resolverMap"
+
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs: [emptyTypeDefs, userTypeDefs],
+    resolvers
+})
+
+export default schema;
