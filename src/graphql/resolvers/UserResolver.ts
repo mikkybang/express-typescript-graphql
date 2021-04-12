fbbb1fc4f7f78397522571c4fe6dfeeb54300f8b
Michael Efemena <mefemena@gmail.com>
2021-04-12 07:07:49 +0100

    add login Query and register Mutation to UserResolver

--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,2 +1,20 @@
 import { IResolvers } from "graphql-tools"
 
+import { AuthenticateResponse, MutationRegisterArgs, QueryLoginArgs } from "../generated"
+
+export const UserResolvers: IResolvers = {
+    Query: {
+        async login(_: void, args: QueryLoginArgs): Promise<AuthenticateResponse> {
+            return {
+                token: "token"
+            }
+        }
+    },
+    Mutation: {
+        async register(_: void, args: MutationRegisterArgs): Promise<AuthenticateResponse> {
+            return {
+                token: "token"
+            }
+        }
+    }
+}
